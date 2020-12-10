ff55327c8b52fbc42bd035320bf1828610130260
machao <machao@babeltime.com>
2020-12-10 14:19:49 +0800

    更新样式

--- pages/contracts/list/list.js
+++ pages/contracts/list/list.js
@@ -90,17 +90,17 @@ Page({
   },
   alreadyShow: function(){
     this.setData({
-      alreadyOrder: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "交易成功", time: "2018-09-30 14:00-16:00", status: "已结束", url: "../../images/bad0.png", money: "132" }, { name: "跃动体育运动俱乐部(圆明园店)", state: "交易成功", time: "2018-10-12 18:00-20:00", status: "未开始", url: "../../images/bad3.jpg", money: "205" }]
+      contracts: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "交易成功", time: "2018-09-30 14:00-16:00", status: "已结束", url: "../../images/bad0.png", money: "132" }, { name: "跃动体育运动俱乐部(圆明园店)", state: "交易成功", time: "2018-10-12 18:00-20:00", status: "未开始", url: "../../images/bad3.jpg", money: "205" }]
     })
   },
   waitPayShow:function(){
     this.setData({
-      waitPayOrder: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "待付款", time: "2018-10-14 14:00-16:00", status: "未开始", url: "../../images/bad1.jpg", money: "186" }],
+      contracts: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "待付款", time: "2018-10-14 14:00-16:00", status: "未开始", url: "../../images/bad1.jpg", money: "186" }],
     })
   },
   lostShow: function () {
     this.setData({
-      lostOrder: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "已取消", time: "2018-10-4 10:00-12:00", status: "未开始", url: "../../images/bad1.jpg", money: "122" }],
+      contracts: [{ name: "跃动体育运动俱乐部(圆明园店)", state: "已取消", time: "2018-10-4 10:00-12:00", status: "未开始", url: "../../images/bad1.jpg", money: "122" }],
     })
   },
 })
--- pages/finances/show/finance.js
+++ pages/finances/show/finance.js
@@ -4,7 +4,34 @@ Page({
   /**
    * 页面的初始数据
    */
+  /**
+   * 页面的初始数据
+   */
   data: {
+    select:false,
+    grade_name:'已逾期',
+    grades: [
+        '快到期',
+        '已到期'
+      ]
+  },/**
+ *  点击下拉框
+ */
+  bindShowMsg() {
+    this.setData({
+      select: !this.data.select
+    })
+  },
+/**
+ * 已选下拉框
+ */
+  mySelect(e) {
+    console.log(e)
+    var name = e.currentTarget.dataset.name
+    this.setData({
+      grade_name: name,
+      select: false
+    })
   },
 
   /**
